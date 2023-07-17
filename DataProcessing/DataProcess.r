e4b6e73f647e111ff6f897be7fd6489afcaf4d4a
Pedro Bento <pbento2000@gmail.com>
2023-07-17 15:13:20 +0100

    data processing

--- DataProcessing/DataProcess.r
+++ DataProcessing/DataProcess.r
@@ -1,5 +1,5 @@
-# install.packages("dplyr", dep=TRUE, rep= "http://cran.uk.r-project.org")
-# install.packages("maditr", dep=TRUE, rep= "http://cran.uk.r-project.org")
+#install.packages("dplyr", dep=TRUE, rep= "http://cran.uk.r-project.org")
+#install.packages("maditr", dep=TRUE, rep= "http://cran.uk.r-project.org")
 suppressMessages(library(dplyr))
 suppressMessages(library(maditr))
 
@@ -11,16 +11,15 @@ print(colnames(events_csv))
 events_csv$companion_Type[events_csv$companion_Type=='0'] <- 'CompSC' 
 events_csv$companion_Type[events_csv$companion_Type=='1'] <- 'CompSF' 
 events_csv$companion_Type[events_csv$companion_Type=='2'] <- 'CompOC' 
-events_csv$companion_Type[events_csv$companion_Type=='3'] <- 'CompOF' 
+events_csv$companion_Type[events_csv$companion_Type=='3'] <- 'CompOF'
 
-resultsLog <- events_csv %>% 
+resultsLog <- subset(events_csv, event_Type!="FINAL")
+
+print(resultsLog)
+
+resultsLog <- resultsLog %>% 
     group_by(study_ID,companion_Type,event_Type,event_Actuator,event_Receiver) %>% 
     summarise(numEvents = n()) 
-
-finalEvents <- events_csv[events_csv$event_Type=='FINAL',]
-finalEvents <- dcast(finalEvents, study_ID ~ companion_Type+event_Type, value.var = "score")
-print(finalEvents)
-# merge(x=resultsLog,y=finalEvents,by=c('study_ID','companion_Type'))
 
 
 # print(head(finalEvents,5))
@@ -29,7 +28,10 @@ print(resultsLog)
 
 resultsLog <- dcast(resultsLog, study_ID ~ companion_Type+event_Type+event_Actuator+event_Receiver, value.var="numEvents")
 
-#MERGE FINAL SCORES WITH "EVENTS_PROCESSED.CSV"
+finalEvents <- events_csv[events_csv$event_Type=='FINAL',]
+finalEvents <- dcast(finalEvents, study_ID ~ companion_Type+event_Type, value.var = "score")
+print(finalEvents)
+resultsLog <- merge(x=resultsLog,y=finalEvents,by=c('study_ID'))
 
 print(resultsLog)
 
